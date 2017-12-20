beeda0e9625d285715042177b83fad9b399f0876
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2017-12-20 12:38:54 -0700

    shell to control generation

--- genbatch.sh
+++ genbatch.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-
-./generate_dijet low o 100  
-./generate_dijet low o 200
-./generate_dijet low o 300  
-./generate_dijet low o 400
-
+cp dijet* olddata/
+rm dijet*
+./generate_dijet low o 100;
+./generate_dijet low o 200;
+./generate_dijet low o 300;
+./generate_dijet low o 400;
+./generate_dijet low o 500;
+./generate_dijet low o 600;
+./generate_dijet low o 700; 
+./generate_dijet low o 800;
+./generate_dijet low o 900;
+./generate_dijet low o 1000;
+./generate_dijet low o 1100;
+./generate_dijet low o 1200;
+./generate_dijet low o 1300;
+./generate_dijet low o 1400;
+./generate_dijet low o 1500;
+./generate_dijet low o 1600;
+./generate_dijet low o 1700; 
+./generate_dijet low o 1800;
+./generate_dijet low o 1900;
+./generate_dijet low o 2000
