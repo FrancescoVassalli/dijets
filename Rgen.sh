6e97cdfc01365dfaca4ba6f35f2c759412104503
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-01-10 13:05:55 -0700

    changed file out

--- Rgen.sh
+++ Rgen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cp Rjet* olddata/
 rm Rjet*
-./generate_Rjets Rjet100 low;
-./generate_Rjets Rjet101 low;
-./generate_Rjets Rjet102 low;
-./generate_Rjets Rjet103 low;
-./generate_Rjets Rjet104 low;
-./generate_Rjets Rjet105 low;
-./generate_Rjets Rjet106 low;
-./generate_Rjets Rjet107 low;
-./generate_Rjets Rjet108 low;
-./generate_Rjets Rjet109 low
+./generate_Rjets Rjet100.root low;
+./generate_Rjets Rjet101.root low;
+./generate_Rjets Rjet102.root low;
+./generate_Rjets Rjet103.root low;
+./generate_Rjets Rjet104.root low;
+./generate_Rjets Rjet105.root low;
+./generate_Rjets Rjet106.root low;
+./generate_Rjets Rjet107.root low;
+./generate_Rjets Rjet108.root low;
+./generate_Rjets Rjet109.root low
