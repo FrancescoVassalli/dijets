6e1925450072b4de83b93f8b0263f9d0d96ae87e
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-01-10 13:00:52 -0700

    changed call

--- Rgen.sh
+++ Rgen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cp Rjet* olddata/
 rm Rjet*
-./generate_dijet Rjet100 low;
-./generate_dijet Rjet101 low;
-./generate_dijet Rjet102 low;
-./generate_dijet Rjet103 low;
-./generate_dijet Rjet104 low;
-./generate_dijet Rjet105 low;
-./generate_dijet Rjet106 low;
-./generate_dijet Rjet107 low;
-./generate_dijet Rjet108 low;
-./generate_dijet Rjet109 low
+./generate_Rjets Rjet100 low;
+./generate_Rjets Rjet101 low;
+./generate_Rjets Rjet102 low;
+./generate_Rjets Rjet103 low;
+./generate_Rjets Rjet104 low;
+./generate_Rjets Rjet105 low;
+./generate_Rjets Rjet106 low;
+./generate_Rjets Rjet107 low;
+./generate_Rjets Rjet108 low;
+./generate_Rjets Rjet109 low
