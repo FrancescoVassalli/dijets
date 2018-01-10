8ff51b698f0d0f31b2051259570a09818d1a4738
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-01-10 12:58:19 -0700

    shell for R jet generation

---
+++ Rgen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp dijet* olddata/
+rm dijet*
+./generate_dijet Rjet100 low;
+./generate_dijet Rjet101 low;
+./generate_dijet Rjet102 low;
+./generate_dijet Rjet103 low;
+./generate_dijet Rjet104 low;
+./generate_dijet Rjet105 low;
+./generate_dijet Rjet106 low;
+./generate_dijet Rjet107 low;
+./generate_dijet Rjet108 low;
+./generate_dijet Rjet109 low
