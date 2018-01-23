e5e61b4c1c49d6ca521c8fed8ad15003d29be556
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-01-23 10:28:51 -0700

    working on split pad

--- FatHist.C
+++ FatHist.C
@@ -47,8 +47,8 @@ void FatHist(int filecount){
   	float bins[] = {.32,.36,.39,.45,.5,.56,.63,.7,.79,.88,1};
   	const int Nbins = 10;
 
-  	int nRbins = 20;
-  	float Rbins[] = {0,.05,.1,.15,.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85,.9,.95,1};
+  	int nRbins = 25;
+  	float Rbins[] = {0,.05,.1,.15,.2,.25,.3,.35,.4,.45,.5,.55,.6,.65,.7,.75,.8,.85,.9,.95,1,1.05,1.1,1.15,1.2,1.25};
   	TH2F *XR = new TH2F("XR", "Xj wrt radius",nRbins, Rbins,Nbins,bins);
   	XR->SetXTitle("radius of wider jet");
   	XR->SetYTitle("Xj ratio");
@@ -65,11 +65,12 @@ void FatHist(int filecount){
   	Jr->Draw();
   	printfile = "JetR"+outfile;
   	tc->Print(printfile.c_str());
-/*
+
     TH1F *rselect;
     std::string selectname = "XjR";
     std::string temp;
-    for(int i=0; i<nRbins+1;i++){
+    float width;
+    for(int i=1; i<nRbins;i++){
       temp = selectname+std::to_string(i*.05+.05);
       rselect= new TH1F("rselect",temp.c_str(),Nbins,bins);
       rselect->SetXTitle("Xj ratio");
@@ -78,9 +79,41 @@ void FatHist(int filecount){
       dijet_tree->Draw(temp.c_str(),"","goff");
       rselect->Sumw2(kTRUE);
       rselect->Scale(1/rselect->Integral());
+      rselect->SetMarkerStyle(20);
+      rselect->SetLineWidth(3);
+      for(int j=0; j<Nbins;j++){
+        width = bins[j+1]-bins[j];
+        rselect->SetBinContent(j+1,rselect->GetBinContent(j+1)/width);
+      }
       rselect->Draw("P");
       printfile = selectname+std::to_string(i)+outfile;
       tc->Print(printfile.c_str());
       delete rselect;
-    }*/
+    }
+
+    delete tc;
+    tc = new TCanvas("tc","1D plots", 1000, 500);
+    tc->Divide(7,3,.01,.02);
+
+    for(int i=1; i<nRbins;i++){
+      tc->cd(i);
+      temp = selectname+std::to_string(i*.05+.05);
+      rselect= new TH1F("rselect",temp.c_str(),Nbins,bins);
+      rselect->SetXTitle("Xj ratio");
+      rselect->SetYTitle("count");
+      temp = selectname+std::to_string(i)+">>rselect";
+      dijet_tree->Draw(temp.c_str(),"","goff");
+      rselect->Sumw2(kTRUE);
+      rselect->Scale(1/rselect->Integral());
+      rselect->SetMarkerStyle(20);
+      rselect->SetLineWidth(3);
+      for(int j=0; j<Nbins;j++){
+        width = bins[j+1]-bins[j];
+        rselect->SetBinContent(j+1,rselect->GetBinContent(j+1)/width);
+      }
+      rselect->Draw("P");
+      delete rselect;
+    }
+    printfile = selectname+outfile;
+    tc->Print(printfile.c_str());
 }
